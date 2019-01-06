ca45e7e77f8f37f1e087d8cbf9f4badfec70ebff
James Green <jrgreen@gmail.com>
2019-01-06 23:37:25 +0000

    Add transparent background to heading

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -20,8 +20,12 @@ export default class HomeScreen extends React.Component {
       <View style={styles.container}>
         <View style={styles.contentContainer}>
           <ImageBackground source={require('../assets/images/kites.png')} style={styles.imageBackground}>
-            <Text style={styles.headingText}>Surviving Me</Text>
-            <Text style={styles.headingSubText}>Living with a life-changing diagnosis</Text>
+            <View style={styles.headingContainer}>
+              <View style={styles.headingTextContainer}>
+                <Text style={styles.headingText}>Surviving Me</Text>
+                <Text style={styles.headingSubText}>Living with a life-changing diagnosis</Text>
+              </View>
+            </View>
           </ImageBackground>
         </View>
       </View>
@@ -44,11 +48,18 @@ const styles = StyleSheet.create({
     flex: 0,
   },
   imageBackground:{
+    width: '100%', 
+    height: '100%',
+  },
+  headingContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    width: '100%', 
-    height: '100%',
+  },
+  headingTextContainer:{
+    padding: 10,
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.15)'
   },
   headingText: {
     color: '#FFF',
@@ -58,5 +69,4 @@ const styles = StyleSheet.create({
     color: '#FFF',
     fontSize: 20,
   },
-
 });
